169173b0874d4bae4ac2660aedbbe06afa667e42
drozzy11 <ruiz.diego071299@gmail.com>
2023-08-24 19:57:40 -0500

    Api stuff hopefully works now

---
+++ client/src/components/locationsearch.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete';
+import '@geoapify/geocoder-autocomplete/styles/minimal.css';
+
+const LocationSearch = () => {
+  function onPlaceSelect(value) {
+    console.log(value);
+  }
+
+  function onSuggestionChange(value) {
+    console.log(value);
+  }
+
+ // add other functions if needed here 
+
+  return (
+    <GeoapifyContext apiKey="2572cdb6e6ff4627b25d413527387788">
+      <GeoapifyGeocoderAutocomplete
+        placeSelect={onPlaceSelect}
+        suggestionsChange={onSuggestionChange}
+        // other props...
+      />
+    </GeoapifyContext>
+  );
+};
+
+export default LocationSearch;
+
+/*  This is the code that need to placed within a page
+import React from 'react';
+import LocationSearch from './components/LocationSearch'; // adjust this path if needed
+
+const MyPage = () => {
+  return (
+    <div>
+      
+      <LocationSearch />
+    
+    </div>
+  );
+};
+
+export default MyPage;
+*/
