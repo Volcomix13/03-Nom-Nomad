7c4ca162dc3979e5e32a1874994724a88b2c774d
drozzy11 <ruiz.diego071299@gmail.com>
2023-08-28 20:25:39 -0500

    CSS works!!

--- server/server.js
+++ server/server.js
@@ -19,11 +19,15 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+
+  // Serve your CSS and other static assets
+  app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
 }
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
