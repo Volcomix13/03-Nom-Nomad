f4c464111df2878611d8e9a07138f41e1c1a4b31
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-31 01:27:28 -0500

    fixed Home page link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 
 
-// import Auth from "../utils/auth";
+//import Auth from "../utils/auth";
 
 const Header = () => {
     // const logout = (event) => {
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import React from 'react';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const Map = () => {
+// const Map = () => {
   
-  const center = [ 37.7749, -122.4194]; 
-  const zoom = 13;
+//   const center = [ 37.7749, -122.4194]; 
+//   const zoom = 13;
 
-  return (
-    <section id="map-sect">
-      <MapContainer center={center} zoom={zoom} style={{ height: '400px', width: '100%' }}>
+//   return (
+//     <section id="map-sect">
+//       <MapContainer center={center} zoom={zoom} style={{ height: '400px', width: '100%' }}>
       
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
+//         <TileLayer
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         />
 
-        { <Marker position={center}>
-          <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-          </Marker>}
+//         { <Marker position={center}>
+//           <Popup>
+//               A pretty CSS3 popup. <br /> Easily customizable.
+//           </Popup>
+//           </Marker>}
 
-        <Marker position={center}>
-          <Popup>
-            A sample location<br />
-            Latitude: {center[0]}<br />
-            Longitude: {center[1]}
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </section>
-  );
-};
+//         <Marker position={center}>
+//           <Popup>
+//             A sample location<br />
+//             Latitude: {center[0]}<br />
+//             Longitude: {center[1]}
+//           </Popup>
+//         </Marker>
+//       </MapContainer>
+//     </section>
+//   );
+// };
 
-export default Map;
+// // export default Map;
