8edfc703b7763ea87738fa69ea47ed80dfeb0e3e
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-30 21:45:15 -0500

    changed dashboard from class to a function

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,53 +1,49 @@
-// import React from 'react';
+import React, { useState } from 'react';
 
-// const Dashboard = () => {
-//     constructor() {
-//         super();
-//         this.state = {
-//         favoriteFoodTrucks: [
-//             { id: 1, name: '', cuisine: '' },
-//             { id: 2, name: '', cuisine: '' },
-//         ],
-//         recentReviews: [
-//             { id: 1, foodTruckName: '', rating: 4, comment: '' },
-//             { id: 2, foodTruckName: '', rating: 5, comment: '' },
-//         ],
-//         };
-//     }
+const Dashboard = () => {
+    const [favoriteFoodTrucks] = useState([
+        { id: 1, name: ' ', cuisine: ' ' }, 
+        { id: 2, name: ' ', cuisine: ' ' }, 
+    ]);
 
-//     render() {
-//         const { favoriteFoodTrucks, recentReviews } = this.state;
+    const [recentReviews] = useState([
+        { id: 1, foodTruckName: ' ', rating: 4, comment: ' ' },
+        { id: 2, foodTruckName: ' ', rating: 5, comment: ' ' },    
+    ]);
 
-//         return (
-//         <main>
-//             <div className="dashboard-container">
-//                 <section className="favorite-food-trucks" id="fav-trucks">
-//                 <h3>Favorites</h3>
-//                 <ul>
-//                 {favoriteFoodTrucks.map((truck) => (
-//                     <li key={truck.id}>
-//                         <strong>{truck.name}</strong> - {truck.cuisine}
-//                     </li>
-//                 ))}
-//                 </ul>
-//                 </section>
+    return (
+        // const { favoriteFoodTrucks, recentReviews } = this.state;
+
+        // return (
+        <div>
+            <div className="dashboard-container">
+                <div className="favorite-food-trucks" id="fav-trucks">
+                <h3>Favorites</h3>
+                <ul>
+                {favoriteFoodTrucks.map((truck) => (
+                    <li key={truck.id}>
+                        <strong>{truck.name}</strong> - {truck.cuisine}
+                    </li>
+                ))}
+                </ul>
+                </div>
         
-//                 <section className="recent-reviews" id="review-container">
-//                 <h3>Reviews</h3>
-//                 <ul>
-//                     {recentReviews.map((review) => (
-//                     <li key={review.id}>
-//                         <strong>{review.foodTruckName}</strong>
-//                         <div>Rating: {review.rating}</div>
-//                         <div>{review.comment}</div>
-//                     </li>
-//                 ))}
-//                 </ul>
-//                 </section>
-//             </div>
-//         </main>
-//         );
-//     }
-// }
+                <div className="recent-reviews" id="review-container">
+                <h3>Reviews</h3>
+                <ul>
+                    {recentReviews.map((review) => (
+                    <li key={review.id}>
+                        <strong>{review.foodTruckName}</strong>
+                        <div>Rating: {review.rating}</div>
+                        <div>{review.comment}</div>
+                    </li>
+                ))}
+                </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
 
-// export default Dashboard;
+
+export default Dashboard;
