4035bc4ff08b783fb67431460a620eac714cc540
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-08-31 01:13:32 -0500

    edited header file

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,14 +18,10 @@ const Header = () => {
                     </Link>
                     <ul>
                         <li><Link to ="/mission">Our Mission</Link></li>
-                        {/* {Auth.loggedIn() ? ( */}
-                        {/* <li><button on onClick={logout}>Logout</button></li>   */}
-                        {/* ) : ( */}
-                        <>
+                        
                         <li><Link to="/login">Login</Link></li>
                         <li><Link to="/signup">Signup</Link></li>
-                        </>
-                        {/* )} */}
+                        
                     </ul>
                 </nav>
                 
